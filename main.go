ff868f8b49d74851ae68afea76fb3b3a0d5d6f90
Andrey Tarantsov <andrey@tarantsov.com>
2016-03-26 23:27:39 +0600

    🚧 main.go skeleton :-)

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "LibreAnalytics"
+	app.Usage = "make an explosive entrance"
+	app.Action = func(c *cli.Context) {
+		println("boom! I say!")
+	}
+
+	app.Run(os.Args)
+}
